0898f07a38486f7c31c92aad993baf8735ab239b
ideaccum <kitagawah79@gmail.com>
2019-03-17 18:24:14 +0900

    ...

---
+++ src/main/java/org/ideaccum/libs/commons/util/ArrayUtil.java
@@ -0,0 +1,25 @@
+package org.ideaccum.libs.commons.util;
+
+/**
+ * 配列に対する操作を行う際の支援的な操作メソッドを提供します。<br>
+ * <p>
+ * 配列操作で利用頻度の高いリスト操作を提供します。<br>
+ * </p>
+ * 
+ * @author Kitagawa<br>
+ * 
+ *<!--
+ * 更新日		更新者			更新内容
+ * 2012/07/26	Kitagawa		新規作成
+ * 2018/05/24	Kitagawa		再構築(SourceForge.jpからGitHubへの移行に併せて全面改訂)
+ *-->
+ */
+public final class ArrayUtil {
+
+	/**
+	 * コンストラクタ<br>
+	 */
+	private ArrayUtil() {
+		super();
+	}
+}
